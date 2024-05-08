60aa34328fa97e5d098e6fd47d348deb57267bb0
Stalin Pereira <letter2stalin@gmail.com>
2024-05-08 19:07:36 +0200

    added absense list card

---
+++ lib/app/modules/absense_manager/views/absense_list_card_view.dart
@@ -0,0 +1,202 @@
+import 'package:cm_flutter/app/helpers/common_functions.dart';
+import 'package:cm_flutter/app/models/absense_list_response.dart';
+import 'package:flutter/material.dart';
+
+class AbsenseListCardView extends StatelessWidget {
+  final AbsenseListModel? absenseListModel;
+  final GestureTapCallback? onTap;
+
+  const AbsenseListCardView({
+    Key? key,
+    this.absenseListModel,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(8),
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AbsenseMemberNameTextWidget(
+                      absenseListModel: absenseListModel),
+                  AbsenseTypeChipWidget(absenseListModel: absenseListModel)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AbsenseStartDateTextWidget(
+                      absenseListModel: absenseListModel),
+                  AbsenseEndDateTextWidget(absenseListModel: absenseListModel),
+                ],
+              ),
+              AbsenseMemberNoteTextWidget(absenseListModel: absenseListModel),
+              AbsenseStatusTextWidget(absenseListModel: absenseListModel),
+              AbsenseAdmitterNoteTextWidget(absenseListModel: absenseListModel),
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
+
+class AbsenseAdmitterNoteTextWidget extends StatelessWidget {
+  const AbsenseAdmitterNoteTextWidget({
+    super.key,
+    required this.absenseListModel,
+  });
+
+  final AbsenseListModel? absenseListModel;
+
+  @override
+  Widget build(BuildContext context) {
+    if (isNotEmpty(absenseListModel?.admitterNote)) {
+      return Text(
+        'Admitter note : ${absenseListModel?.admitterNote ?? ""}',
+        style: Theme.of(context).textTheme.bodyMedium,
+        maxLines: 2,
+      );
+    } else {
+      return SizedBox.shrink();
+    }
+  }
+}
+
+class AbsenseStatusTextWidget extends StatelessWidget {
+  const AbsenseStatusTextWidget({
+    super.key,
+    required this.absenseListModel,
+  });
+
+  final AbsenseListModel? absenseListModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Status : ${getAbsenseStatus(absenseListModel: absenseListModel)}',
+      style: Theme.of(context).textTheme.bodyMedium,
+      maxLines: 2,
+    );
+  }
+}
+
+class AbsenseMemberNoteTextWidget extends StatelessWidget {
+  const AbsenseMemberNoteTextWidget({
+    super.key,
+    required this.absenseListModel,
+  });
+
+  final AbsenseListModel? absenseListModel;
+
+  @override
+  Widget build(BuildContext context) {
+    if (isNotEmpty(absenseListModel?.memberNote)) {
+      return Text(
+        'Member note : ${absenseListModel?.memberNote ?? ""}',
+        style: Theme.of(context).textTheme.bodyMedium,
+        maxLines: 2,
+      );
+    } else {
+      return SizedBox.shrink();
+    }
+  }
+}
+
+class AbsenseEndDateTextWidget extends StatelessWidget {
+  const AbsenseEndDateTextWidget({
+    super.key,
+    required this.absenseListModel,
+  });
+
+  final AbsenseListModel? absenseListModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'To : ${absenseListModel?.endDate ?? ""}',
+      style: Theme.of(context).textTheme.bodyMedium,
+      maxLines: 2,
+    );
+  }
+}
+
+class AbsenseStartDateTextWidget extends StatelessWidget {
+  const AbsenseStartDateTextWidget({
+    super.key,
+    required this.absenseListModel,
+  });
+
+  final AbsenseListModel? absenseListModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'From : ${absenseListModel?.startDate ?? ""}',
+      style: Theme.of(context).textTheme.bodyMedium,
+      maxLines: 2,
+    );
+  }
+}
+
+class AbsenseTypeChipWidget extends StatelessWidget {
+  const AbsenseTypeChipWidget({
+    super.key,
+    required this.absenseListModel,
+  });
+
+  final AbsenseListModel? absenseListModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      padding: EdgeInsets.zero,
+      side: BorderSide(
+          color:
+              absenseListModel?.type == "vacation" ? Colors.green : Colors.red),
+      backgroundColor: absenseListModel?.type == "vacation"
+          ? Colors.green.withOpacity(0.1)
+          : Colors.red.withOpacity(0.1),
+      label: Text(
+        absenseListModel?.type ?? "",
+        style: TextStyle(
+            color: absenseListModel?.type == "vacation"
+                ? Colors.green
+                : Colors.red),
+      ),
+    );
+  }
+}
+
+class AbsenseMemberNameTextWidget extends StatelessWidget {
+  const AbsenseMemberNameTextWidget({
+    super.key,
+    required this.absenseListModel,
+  });
+
+  final AbsenseListModel? absenseListModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      absenseListModel?.userDoc?.name ?? "",
+      style: Theme.of(context).textTheme.titleLarge,
+      // overflow: TextOverflow.ellipsis,
+    );
+  }
+}
