8781dd54e125a3df2d6e836276b1d98cf93bc66b
Stalin Pereira <letter2stalin@gmail.com>
2024-05-08 19:08:25 +0200

    added list page logic with controller

--- lib/app/modules/absense_manager/controllers/absense_manager_list_ctrl.dart
+++ lib/app/modules/absense_manager/controllers/absense_manager_list_ctrl.dart
@@ -1,12 +1,113 @@
+import 'package:cm_flutter/app/models/absense_list_request.dart';
+import 'package:cm_flutter/app/models/absense_list_response.dart';
+import 'package:cm_flutter/app/modules/absense_manager/providers/absense_list_provider.dart';
+import 'package:cm_flutter/app/system/result.dart';
+import 'package:cm_flutter/app/utils/message_displayer.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class AbsenseManagerListController extends GetxController {
-  //TODO: Implement HomeController
+  final types = ['sickness', 'vacation'];
 
-  final count = 0.obs;
+  late AbsenseListProvider absenseListProvider;
+
+  var absenseListRequest = AbsenseListRequest().obs;
+  var absenseListResponse = AbsenseListResponse().obs;
+
+  // for absense list pagination
+  int pageSize = 10;
+  bool isAbsenseListLastPage = false;
+  final PagingController<int, AbsenseListModel> absenseListPagingController =
+      PagingController(firstPageKey: 1);
+
   @override
   void onInit() {
+    absenseListProvider = Get.put(AbsenseListProvider());
+    absenseListPagingController.addPageRequestListener((pageKey) {
+      if (!isAbsenseListLastPage) {
+        getAbsenseListData(page: pageKey);
+      }
+    });
+
     super.onInit();
+  }
+
+  Future<void> onDatePickerBtnClick(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: absenseListRequest.value.selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != absenseListRequest.value.selectedDate) {
+      absenseListRequest.value.selectedDate = picked;
+      absenseListRequest.refresh();
+      absenseListPagingController.itemList?.clear();
+      // absenseListPagingController.refresh();
+      getAbsenseListData(page: 1);
+    }
+  }
+
+  setSelectedType(String? value) {
+    absenseListRequest.value.absenceType = value;
+    absenseListRequest.refresh();
+    absenseListPagingController.itemList?.clear();
+    // absenseListPagingController.refresh();
+    getAbsenseListData(page: 1);
+  }
+
+  onClearFilterBtnClick() {
+    absenseListRequest.value = AbsenseListRequest();
+    absenseListPagingController.refresh();
+    getAbsenseListData(page: 1);
+  }
+
+  void getAbsenseListData({int page = 1}) async {
+    absenseListRequest.value.page = page;
+    absenseListRequest.value.toDate = absenseListRequest.value.selectedDate
+        ?.add(Duration(hours: 23, minutes: 59))
+        .toIso8601String();
+    absenseListRequest.value.fromDate =
+        absenseListRequest.value.selectedDate?.toIso8601String();
+    absenseListRequest.value.pageSize = pageSize;
+    Result<AbsenseListResponse, Exception, Exception, Exception> result =
+        await absenseListProvider.getAbsenseList(absenseListRequest.value);
+
+    final apiResultFn = switch (result) {
+      Success(value: final res) => () {
+          isAbsenseListLastPage = page >= (res.totalPages ?? 1);
+
+          if (isAbsenseListLastPage) {
+            absenseListPagingController.appendLastPage(res.rows!);
+          } else {
+            absenseListPagingController.appendPage(res.rows!, page + 1);
+          }
+          absenseListResponse.value = res;
+        },
+      ApiFailure(exception: final exception, metadata: final meta) => () {
+          if (meta.code == 401) {
+            Get.showSnackbar(GetSnackBar(
+              title: meta.resBody.message,
+              message: 'Something went wrong: $exception',
+              duration: const Duration(seconds: 3),
+              backgroundColor: Colors.red,
+            ));
+          } else {
+            MessageDisplayer.apiFailureMessageDisplay(exception);
+          }
+          absenseListPagingController.error = exception;
+        },
+      ValidationFailure(exception: final exception) => () {
+          MessageDisplayer.validationFailureMessageDisplay(exception);
+          absenseListPagingController.error = exception;
+        },
+      NewtorkFailure(exception: final exception) => () {
+          MessageDisplayer.networkFailureMessageDisplay(exception);
+          absenseListPagingController.error = exception;
+        },
+    };
+
+    apiResultFn();
   }
 
   @override
@@ -18,6 +119,4 @@ class AbsenseManagerListController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/absense_manager/views/absense_manager_list_pg.dart
+++ lib/app/modules/absense_manager/views/absense_manager_list_pg.dart
@@ -1,24 +1,118 @@
+import 'package:cm_flutter/app/models/absense_list_response.dart';
 import 'package:cm_flutter/app/modules/absense_manager/controllers/absense_manager_list_ctrl.dart';
+import 'package:cm_flutter/app/modules/absense_manager/views/absense_list_card_view.dart';
+import 'package:cm_flutter/app/modules/absense_manager/views/custom_date_picker_btn_view.dart';
+import 'package:cm_flutter/app/modules/absense_manager/views/custom_drop_down_btn_view.dart';
+import 'package:cm_flutter/app/modules/absense_manager/views/empty_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:intl/intl.dart';
 
 class AbsenseManagerListPage extends StatelessWidget {
-  const AbsenseManagerListPage({Key? key}) : super(key: key);
+  const AbsenseManagerListPage({super.key});
+
   // const AbsenseManagerListPage({super.key});
   @override
   Widget build(context) {
     AbsenseManagerListController absenseManagerListController =
-        Get.put(AbsenseManagerListController());
+        Get.isRegistered<AbsenseManagerListController>()
+            ? Get.find<AbsenseManagerListController>()
+            : Get.put(AbsenseManagerListController());
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Absense List'),
+      appBar: AppBar(
+        title: Text("Absense List"),
+        scrolledUnderElevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Obx(() {
+                  return CustomDatepickerBtnView(
+                    onTap: () {
+                      absenseManagerListController
+                          .onDatePickerBtnClick(context);
+                    },
+                    label: absenseManagerListController
+                                .absenseListRequest.value.selectedDate ==
+                            null
+                        ? "Select Date"
+                        : DateFormat('yyyy-MM-dd').format(
+                            absenseManagerListController
+                                .absenseListRequest.value.selectedDate!),
+                  );
+                }),
+                Obx(() {
+                  return CustomDropdownBtnView(
+                    currentValue: absenseManagerListController
+                        .absenseListRequest.value.absenceType,
+                    values: absenseManagerListController.types,
+                    onChanged: (value) {
+                      absenseManagerListController.setSelectedType(value);
+                    },
+                  );
+                }),
+                TextButton(
+                    onPressed: () {
+                      absenseManagerListController.onClearFilterBtnClick();
+                    },
+                    child: const Text("Clear"))
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Obx(() {
+              if (absenseManagerListController
+                      .absenseListResponse.value.total !=
+                  0) {
+                return Text(
+                    'Loaded ${absenseManagerListController.absenseListRequest.value.page ?? 0} of ${absenseManagerListController.absenseListResponse.value.totalPages ?? ''} page , Total ${absenseManagerListController.absenseListResponse.value.total ?? ''} records');
+              } else {
+                return SizedBox.shrink();
+              }
+            }),
+            Flexible(
+              child: PagedGridView<int, AbsenseListModel>(
+                showNewPageProgressIndicatorAsGridChild: true,
+                showNewPageErrorIndicatorAsGridChild: true,
+                showNoMoreItemsIndicatorAsGridChild: true,
+                shrinkWrap: true,
+                pagingController:
+                    absenseManagerListController.absenseListPagingController,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  childAspectRatio: 400 / 250,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  maxCrossAxisExtent: 400,
+                  // crossAxisCount: 5,
+                ),
+                builderDelegate: PagedChildBuilderDelegate<AbsenseListModel>(
+                    itemBuilder: (context, absenseListModel, index) =>
+                        AbsenseListCardView(
+                            absenseListModel: absenseListModel, onTap: () {}),
+                    firstPageErrorIndicatorBuilder: (context) {
+                      return const Center(
+                          child: Text("Oops! something went wrong!"));
+                    },
+                    noItemsFoundIndicatorBuilder: (context) {
+                      return EmptyListView(
+                        onClearFilterBtnClick: () {
+                          absenseManagerListController.onClearFilterBtnClick();
+                        },
+                      );
+                    }),
+              ),
+            ),
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Text("Hello"),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: (){},
-            child: const Icon(Icons.filter_list_rounded)));
+      ),
+    );
   }
 }
