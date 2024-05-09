b4c5056be66e88cf2f6ca538bd8cde4489891265
Stalin Pereira <letter2stalin@gmail.com>
2024-05-09 10:23:52 +0200

    added test for status checking

--- test/absense_list_card_test.dart
+++ test/absense_list_card_test.dart
@@ -57,6 +57,81 @@ void main() {
                 "name": "Mike",
                 "userId": 2664
             }
+        },
+        {
+            "admitterId": null,
+            "admitterNote": "",
+            "confirmedAt": "2020-12-12T18:03:55.000+01:00",
+            "createdAt": "2020-12-12T14:17:01.000+01:00",
+            "crewId": 352,
+            "endDate": "2021-01-13",
+            "fmtEndDate": "2021-01-13T00:00:00.000Z",
+            "id": 2351,
+            "memberNote": "",
+            "rejectedAt": null,
+            "startDate": "2021-01-13",
+            "fmtStartDate": "2021-01-13T00:00:00.000Z",
+            "fmtConfirmedAt": "2020-12-12T17:03:55.000Z",
+            "fmtCreatedAt": "2020-12-12T13:17:01.000Z",
+            "type": "sickness",
+            "userId": 2664,
+            "userDoc": {
+                "crewId": 352,
+                "id": 2650,
+                "image": "https://loremflickr.com/300/400",
+                "name": "Mike",
+                "userId": 2664
+            }
+        },
+        {
+            "admitterId": null,
+            "admitterNote": "Sorry",
+            "confirmedAt": null,
+            "createdAt": "2021-01-03T17:36:52.000+01:00",
+            "crewId": 352,
+            "endDate": "2021-01-05",
+            "fmtEndDate": "2021-01-05T00:00:00.000Z",
+            "id": 2521,
+            "memberNote": "ganzer tag",
+            "rejectedAt": "2021-01-03T17:39:50.000+01:00",
+            "startDate": "2021-01-05",
+            "fmtStartDate": "2021-01-05T00:00:00.000Z",
+            "fmtConfirmedAt": "1970-01-01T00:00:00.000Z",
+            "fmtCreatedAt": "2021-01-03T16:36:52.000Z",
+            "type": "vacation",
+            "userId": 2664,
+            "userDoc": {
+                "crewId": 352,
+                "id": 2650,
+                "image": "https://loremflickr.com/300/400",
+                "name": "Mike",
+                "userId": 2664
+            }
+        },
+        {
+            "admitterId": 709,
+            "admitterNote": "",
+            "confirmedAt": null,
+            "createdAt": "2021-05-12T09:27:30.000+02:00",
+            "crewId": 352,
+            "endDate": "2021-06-26",
+            "fmtEndDate": "2021-06-26T00:00:00.000Z",
+            "id": 5291,
+            "memberNote": "",
+            "rejectedAt": null,
+            "startDate": "2021-06-26",
+            "fmtStartDate": "2021-06-26T00:00:00.000Z",
+            "fmtConfirmedAt": "1970-01-01T00:00:00.000Z",
+            "fmtCreatedAt": "2021-05-12T07:27:30.000Z",
+            "type": "sickness",
+            "userId": 8007,
+            "userDoc": {
+                "crewId": 352,
+                "id": 8442,
+                "image": "https://loremflickr.com/300/400",
+                "name": "Linda",
+                "userId": 8007
+            }
         }
     ],
     "total": 42,
@@ -152,4 +227,84 @@ void main() {
 
     expect(textFinder, findsOneWidget);
   });
+
+
+  testWidgets('Absense List Card should not have Admitter Note if no value', (tester) async {
+    AbsenseListModel absenseListModel = absenseListResponse.rows!.elementAt(2);
+
+    await tester.pumpWidget(MaterialApp(
+      home: AbsenseListCardView(absenseListModel: absenseListModel),
+    ));
+
+    final textFinder =
+        find.text('Admitter note : ${absenseListModel.admitterNote ?? ""}');
+
+    expect(textFinder, findsNothing);
+  });
+
+  testWidgets('Absense List Card should not have Member Note if no value', (tester) async {
+    AbsenseListModel absenseListModel = absenseListResponse.rows!.elementAt(2);
+
+    await tester.pumpWidget(MaterialApp(
+      home: AbsenseListCardView(absenseListModel: absenseListModel),
+    ));
+
+    final textFinder =
+        find.text('Member note : ${absenseListModel.memberNote ?? ""}');
+
+    expect(textFinder, findsNothing);
+  });
+
+  testWidgets('Absense List Card should have Status Confirmed', (tester) async {
+    AbsenseListModel absenseListModel = absenseListResponse.rows!.elementAt(2);
+
+    await tester.pumpWidget(MaterialApp(
+      home: AbsenseListCardView(absenseListModel: absenseListModel),
+    ));
+
+    final textFinder =
+        find.text('Status : Confirmed');
+
+    expect(textFinder, findsOneWidget);
+  });
+
+  testWidgets('Absense List Card should have Status Confirmed', (tester) async {
+    AbsenseListModel absenseListModel = absenseListResponse.rows!.elementAt(2);
+
+    await tester.pumpWidget(MaterialApp(
+      home: AbsenseListCardView(absenseListModel: absenseListModel),
+    ));
+
+    final textFinder =
+        find.text('Status : Confirmed');
+
+    expect(textFinder, findsOneWidget);
+  });
+
+  testWidgets('Absense List Card should have Status Rejected', (tester) async {
+    AbsenseListModel absenseListModel = absenseListResponse.rows!.elementAt(3);
+
+    await tester.pumpWidget(MaterialApp(
+      home: AbsenseListCardView(absenseListModel: absenseListModel),
+    ));
+
+    final textFinder =
+        find.text('Status : Rejected');
+
+    expect(textFinder, findsOneWidget);
+  });
+
+  testWidgets('Absense List Card should have Status Requested', (tester) async {
+    AbsenseListModel absenseListModel = absenseListResponse.rows!.elementAt(4);
+
+    await tester.pumpWidget(MaterialApp(
+      home: AbsenseListCardView(absenseListModel: absenseListModel),
+    ));
+
+    final textFinder =
+        find.text('Status : Requested');
+
+    expect(textFinder, findsOneWidget);
+  });
+
 }
